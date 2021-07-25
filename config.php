57b325955340e9f7c293395f28625d4af11df70d
MDRafi08642 <81744420+MDRafi08642@users.noreply.github.com>
2021-07-25 22:42:16 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+// JANGAN MENGUBAH APAPUN SELAIN YG DIDALAM TANDA (")
+// PERHATIKAN TANDA (") JANGAN SAMPAI TERHAPUS
+// AGAR SCRIPT TIDAK ERROR
+
+$user = "Mozilla/5.0 (Linux; Android 11; RMX2193) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36";
+
+$wallet = "D8U3SvLyEyWyS7NqUYaPMSC9ocaYipZFQP";
+
+
+
+
+
+
+
